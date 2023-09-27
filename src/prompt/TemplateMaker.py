705707564917226bcd418e353f8d9a01911dcc0b
lee seonguk <passion1014@gmail.com>
2023-09-28 00:31:35 +0900

    feature prompt template

--- src/prompt/TemplateMaker.py
+++ src/prompt/TemplateMaker.py
@@ -1,8 +1,59 @@
+from re import template
+import sqlite3
 
 class TemplateMaker:
     def __init__(self):
-        pass
+        self.conn = sqlite3.connect('mydatabase.db')
+        self.typeCd = ""
+        self.content = ""
 
-    def selectTemplate(self, typeCd):
-        pass
+    # PromptTemplate 조회
+    def getTemplate(self):
+        pt = ""
+        try:
+            # 특정 typeCd 값을 조건으로 사용하여 데이터 조회
+            cursor = self.conn.cursor()
+            cursor.execute("SELECT * FROM PROMPT_TEMPLATE WHERE type = ?", (self.typeCd,))
+            rows = cursor.fetchall()
+            
+            pt = rows[0][2]
+            print("content: ", rows[0][2])
+
+            # 조회된 데이터 출력
+            #for row in rows:
+            #    print("ID:", row[0])
+            #    print("typeCd:", row[1])
+            #    print("Content:", row[2])
+
+        except sqlite3.Error as e:
+            print("SQLite error:", e)
+
+        finally:
+            # 데이터베이스 연결 종료
+            self.conn.close()
+        
+        return pt
+
+
+    # PromptTemplate 조회하여 content 내용 조합해서 return
+    def makeTemplateText(self, typeCd, content):
+        self.typeCd = typeCd
+        self.content = content
+
+        # PromptTemplate 조회
+        pt = self.getTemplate()
+
+        # 조회한 template에 입력된 값을 셋팅하여 반환한다.
+        templateText = pt.format(content)
+
+        return templateText
+
+
+
+
+
+
+# 사용예제
+template = TemplateMaker()
+print (template.makeTemplateText('CATEGORY','이번에 삼성화재의 실손 보험을 가입하려고 하는데 가능한지 확인해줘 '))
 
--- src/prompt/test_create_table.py
+++ src/prompt/test_create_table.py
@@ -6,6 +6,10 @@ conn = sqlite3.connect('mydatabase.db')
 # 커서 생성
 cursor = conn.cursor()
 
+# 테이블 삭제
+cursor.execute("DROP TABLE IF EXISTS PROMPT_TEMPLATE")
+conn.commit()
+
 # 테이블 생성 (한 번만 실행)
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS PROMPT_TEMPLATE (
@@ -16,8 +20,19 @@ cursor.execute('''
 ''')
 
 # 데이터 추가
-cursor.execute("INSERT INTO PROMPT_TEMPLATE (type, content) VALUES (?, ?)", ("alice", "alice@example.com"))
-cursor.execute("INSERT INTO PROMPT_TEMPLATE (type, content) VALUES (?, ?)", ("bob", "bob@example.com"))
+content = '''
+아래 질문이 어떤 카테고리에 속하는지 알려줘
+
+1. 보험 가입 가능 확인
+2. 보상 여부 확인
+3. 담보나 특약에 대한 질문
+4. 그외
+
+설명하지 않고 답변만 해줘
+
+질문 : {0}
+'''
+cursor.execute("INSERT INTO PROMPT_TEMPLATE (type, content) VALUES (?, ?)", ("CATEGORY", content))
 conn.commit()
 
 # 데이터 조회
