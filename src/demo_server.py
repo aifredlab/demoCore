896874dbaeaed99f8b71ed39b488ba10a41eb59b
lee seonguk <passion1014@gmail.com>
2023-10-19 23:14:29 +0900

    port value from configuration

--- src/demo_server.py
+++ src/demo_server.py
@@ -4,6 +4,7 @@ import logging
 import grpc
 import ask_pb2 
 import ask_pb2_grpc
+import os
 
 from main import Aifred
 
@@ -13,7 +14,7 @@ class Asker(ask_pb2_grpc.AskerServicer):
         return ask_pb2.AskReply(**result)
 
 def serve():
-    port = "50051"
+    port = os.environ.get('SERVER_PORT'),
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     ask_pb2_grpc.add_AskerServicer_to_server(Asker(), server)
     server.add_insecure_port("[::]:" + port)
